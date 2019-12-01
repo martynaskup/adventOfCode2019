8eb0697f349a854c6d2aed8c0830fbc3f9de4ff9
Martyna Skup <martyna.skup@yahoo.de>
2019-12-01 20:34:32 +0100

    Solve the day 01 puzzle - part 2

--- 01/01_puzzle.js
+++ 01/01_puzzle.js
@@ -1,15 +1,38 @@
 // myPuzzle comes from js file with puzzle data
 const puzzle = myPuzzle;
 
-// Part 1
-const allFuel = puzzle.reduce((acc, mass) => {
-  let massFuel = Math.floor(mass / 3) - 2;
-  return acc + massFuel;
+let massFuel = mass => Math.floor(mass / 3) - 2;
+
+// PART 1
+const allFuelPart1 = puzzle.reduce((acc, mass) => {
+  return acc + massFuel(mass);
 }, 0);
-console.log(allFuel);
+console.log('allFuelPart1: ', allFuelPart1);
+
+// PART 2
+const allFuelPart2 = puzzle.reduce((acc, mass) => {
+  // calculate the total fuel for a separate mass from the array
+  let allMassFuel = 0;
+  let calcFuel = mass => {
+    let fuelForThisMass = massFuel(mass);
+
+    // stop executing the function if fuel is <= 0
+    if (fuelForThisMass <= 0) {
+      return;
+    }
+
+    allMassFuel += fuelForThisMass;
+    calcFuel(fuelForThisMass);
+  };
+  calcFuel(mass);
+
+  // add the separate total mass fuel to the total fuel
+  return (acc += allMassFuel);
+}, 0);
+console.log('allFuelPart2: ', allFuelPart2);
 
 // add the solutions to the HTML
 const sol1 = document.getElementById('sol1');
 const sol2 = document.getElementById('sol2');
-sol1.innerHTML += ` <b>${allFuel}</b>`;
-sol2.innerHTML += ` <b>  </b>`;
+sol1.innerHTML += ` <b>${allFuelPart1}</b>`; // 3371958
+sol2.innerHTML += ` <b>${allFuelPart2}</b>`; // 5055050
