2996f58f9e3480b5397dbb1c875d7fb2891a459d
Martyna Skup <martyna.skup@yahoo.de>
2019-12-04 23:30:03 +0100

    Create reusable functions by refactoring the Part 1 code

--- 02/02_puzzle.js
+++ 02/02_puzzle.js
@@ -1,41 +1,57 @@
 console.log('Origin puzzleData', puzzleData);
 
-// PART 1
-// replace the pos 1 and 2 with values 12 before running the program
-const puzzleDataPart1 = puzzleData;
-puzzleDataPart1[1] = 12;
-puzzleDataPart1[2] = 2;
+const puzzleEnd = puzzleData.length;
 
-// change the puzzleDataPart1 accordingly to opcode
-const puzzleEnd = puzzleDataPart1.length;
-for (i = 0; i < puzzleEnd; i = i + 4) {
-  let opcode = puzzleDataPart1[i];
-  let first = puzzleDataPart1[i + 1];
-  let second = puzzleDataPart1[i + 2];
-  let position = puzzleDataPart1[i + 3];
-
-  if (opcode === 1) {
-    puzzleDataPart1[position] =
-      puzzleDataPart1[first] + puzzleDataPart1[second];
-  } else if (opcode === 2) {
-    puzzleDataPart1[position] =
-      puzzleDataPart1[first] * puzzleDataPart1[second];
-  } else if (opcode === 99) {
+// change the puzzle array accordingly to opcode
+function steps(puzzle, pointer, first, second, position) {
+  if (pointer === 1) {
+    puzzle[position] = puzzle[first] + puzzle[second];
+  } else if (pointer === 2) {
+    puzzle[position] = puzzle[first] * puzzle[second];
+  } else if (pointer === 99) {
     i = puzzleEnd - 4;
   } else {
     console.log('the opcode is invalid (nor 1 nor 2 nor 99');
   }
 }
 
+// PART 1
+// clone the original puzzleData array
+const puzzleDataPart1 = puzzleData.slice();
+
+// replace the pos 1 and 2 with values 12 before running the program
+puzzleDataPart1[1] = 12;
+puzzleDataPart1[2] = 2;
+
+// find the end value for the 0-position in the puzzle Array
+// after mapping through the array
+function puzzleZero(puzzles) {
+  for (i = 0; i < puzzleEnd; i = i + 4) {
+    let opcode = puzzles[i];
+    let one = puzzles[i + 1];
+    let two = puzzles[i + 2];
+    let three = puzzles[i + 3];
+    steps(puzzles, opcode, one, two, three);
+  }
+}
+puzzleZero(puzzleDataPart1);
+
 console.log('Endversion of puzzledataPart1', puzzleDataPart1);
 console.log('puzzleDataPart1[0] is ', puzzleDataPart1[0]);
 
 // PART 2
-let noun = puzzleDataPart1[1];
-let verb = puzzleDataPart1[2];
+// output to be found caused by (noun + verb)
+const output = 19690720;
+let puzzleDataPart2 = puzzleData;
+
+let noun = puzzleDataPart2[1];
+let verb = puzzleDataPart2[2];
+let solutionPart2 = 100 * noun + verb;
+
+console.log(puzzleData);
 
 // add the solutions to the HTML
 const sol1 = document.getElementById('sol1');
 const sol2 = document.getElementById('sol2');
 sol1.innerHTML += ` <b>${puzzleDataPart1[0]}</b>`; // 3716293
-sol2.innerHTML += ` <b></b>`; // result
+sol2.innerHTML += ` <b>${solutionPart2}</b>`; // result
